0c9cf989a6d19c26bb73cd720bfeabb206bff22f
NaccOll <wuyoubin0504@gmail.com>
2020-09-04 13:51:36 +0800

    feat: 清除pyc文件

--- clean_pyc.py
+++ clean_pyc.py
@@ -1,7 +1,10 @@
 import os
-path = __file__
+path = '.'
+env_dir = (path+os.path.sep+'.venv')
 for prefix, dirs, files in os.walk(path):
     for name in files:
         if name.endswith('.pyc'):
             filename = os.path.join(prefix, name)
+            if filename.startswith(env_dir):
+                continue
             os.remove(filename)
